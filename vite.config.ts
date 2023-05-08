448512ea3ae361ee2de12095ee1ab379dfd2868d
lusmattg <lusmattg@users.noreply.github.com>
2023-05-08 16:30:04 -0400

    implement basic tailwind config and classes

--- vite.config.ts
+++ vite.config.ts
@@ -13,5 +13,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    require('tailwindcss'),
+    require('autoprefixer'),
+    require('postcss-flexbugs-fixes'),
+    require('postcss-preset-env')
+  ],
 })
