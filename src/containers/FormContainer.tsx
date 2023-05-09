21f29abc0a7b2a8c5702f787184be455d51989da
Jeb Stone <jeb.b.stone@gmail.com>
2023-05-09 16:34:28 -0700

    updates to interfaces.ts
    Co-authored-by: Jaysalgado <Jaysalgado@users.noreply.github.com>
    Co-authored-by: annamullike <annamullike@users.noreply.github.com>
    Co-authored-by: lusmattg <lusmattg@users.noreply.github.com>
    Co-authored-by: Natalie Cordoves <ncordoves@users.noreply.github.com>

--- src/containers/FormContainer.tsx
+++ src/containers/FormContainer.tsx
@@ -1,17 +1,36 @@
-import React, { Component, ReactComponentElement } from "react";
+import React, { useState } from "react";
+import { useNavigate } from 'react-router-dom';
 import SignUpForm  from '../components/SignUpForm';
 import LoginForm  from '../components/LoginForm';
+import { LogInFormValues } from '../../types/interfaces';
+import { SignUpFormValues } from "../../types/interfaces";
+import { LogInProps } from "../../types/interfaces";
+
 // import { v4 as uuidv4 } from 'uuid';
 
 type SelectProps = {
-  formType: string;
-  
+  formType: string; 
 }
+
 const FormContainer: React.FC<SelectProps> = ( { formType } ) => {
+
+  const [loginValues, setLoginValues] = useState<LogInFormValues>({
+    username: '', 
+    password: ''
+  });
+  const [signupValues, setSignUpValues] = useState<SignUpFormValues>({})
+
+    const navigate = useNavigate();
   // pass in correct form
-  const form = formType === 'sign-up-form' ? <SignUpForm/> : <LoginForm/>
+  const form = formType === 'sign-up-form' ?  
+    <SignUpForm
+      setSignupValues={setSignUpValues}
+    /> 
+    : 
+    <LoginForm
+    />
   return (
-    <div className="w-full max-w-xs">
+    <div className="w-full max-w-sm border p-5">
       Form Container
       {form}
       <p className="text-center text-gray-500 text-xs">
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'regal-blue': '#243c5a',
+      }
+    },
   },
   plugins: [],
 }
